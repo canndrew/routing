maidsafe/sn_routing

feat/crust: update names
Use compatibility layer.

src/lib.rs
@@ -249,9 +249,9 @@ pub use routing_table::verify_network_invariant;
 pub use types::MessageId;
 pub use xor_name::{XOR_NAME_BITS, XOR_NAME_LEN, XorName, XorNameFromHexError};
 
-type Service = crust::Service<PublicId>;
-use crust::Event as CrustEvent;
-type CrustEventSender = crust::CrustEventSender<PublicId>;
+type Service = crust::compat::Service<PublicId>;
+use crust::compat::Event as CrustEvent;
+type CrustEventSender = crust::compat::CrustEventSender<PublicId>;
 type PrivConnectionInfo = crust::PrivConnectionInfo<PublicId>;
 type PubConnectionInfo = crust::PubConnectionInfo<PublicId>;
 

canndrew/routing

src/lib.rs
@@ -244,9 +244,9 @@ pub use routing_table::verify_network_invariant;
 pub use types::MessageId;
 pub use xor_name::{XOR_NAME_BITS, XOR_NAME_LEN, XorName, XorNameFromHexError};
 
-type Service = crust::Service<PublicInfo>;
-use crust::Event as CrustEvent;
-type CrustEventSender = crust::CrustEventSender<PublicInfo>;
+type Service = crust::compat::Service<PublicInfo>;
+use crust::compat::Event as CrustEvent;
+type CrustEventSender = crust::compat::CrustEventSender<PublicInfo>;
 type PrivConnectionInfo = crust::PrivConnectionInfo<PublicInfo>;
 type PubConnectionInfo = crust::PubConnectionInfo<PublicInfo>;
 